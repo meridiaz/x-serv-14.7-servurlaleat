b5198c280bd69df45f9d4dc10932de93b4105224
meri <mdiaz>
2020-02-22 13:34:07 +0100

    añado el codigo del 14.3

--- urls_aleat.py
+++ urls_aleat.py
@@ -3,11 +3,12 @@ import webapp
 
 class urlsAleat(webapp.webApp):
     def process(self, parsedRequest):
-        return ("200 OK", b"<html><body><h1>Hello World!</h1>" +
-                            b"<p><a href='"+ bytes(str(aleat), 'utf-8') +
-                            b"'>Dame otra</a>" +
-                            b"</p>" +
-                            b"</body></html>")
+        aleat = random.randint(0,1000)
+        return ("200 OK", "<html><body><h1>Hello World!</h1>" +
+                            "<p><a href='"+ bytes(str(aleat), 'utf-8') +
+                            "'>Dame otra</a>" +
+                            "</p>" +
+                            "</body></html>")
 
 if __name__ == "__main__":
     testWebApp_hola = urlsAleat("localhost", 1234)
